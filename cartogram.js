74612387acac2a616d36e0903fc3ab762f473e99
Wesley Chen <wesleychen@college.harvard.edu>
2014-04-28 02:31:16 -0400

    updates

--- cartogram.js
+++ cartogram.js
@@ -155,7 +155,7 @@
     }
 
     var iterations = 8,
-        projection = d3.geo.mercator(),
+        projection = d3.geo.albers(),
         properties = function(id) {
           return {};
         },
